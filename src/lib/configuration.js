dea9bf4a38c9a6c100a712680fb259d48ef884ba
savad <savadsvd088@gmail.com>
2025-07-01 12:45:38 +0530

    conf change

--- src/lib/configuration.js
+++ src/lib/configuration.js
@@ -1,4 +1,2 @@
-// export const API_BASE_URL = "https://myapp8952.loca.lt"
 // export const API_BASE_URL = "http://localhost:5002"
 export const API_BASE_URL = "http://192.168.1.13:5002"
-// export const API_BASE_URL = "https://3.135.189.191:5002"
